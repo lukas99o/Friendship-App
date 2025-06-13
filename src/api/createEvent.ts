c12996c3134ae43842a9428cecd009a05128bf38
Lukas Olsson <Lukas99O@hotmail.com>
2025-06-13 16:20:09 +0200

    Nu är skapa event klar

--- src/api/createEvent.ts
+++ src/api/createEvent.ts
@@ -5,6 +5,16 @@ export default async function CreateEvent(parameters: any) {
             "Content-Type": "application/json",
             "Authorization": `Bearer ${localStorage.getItem("jwtToken")}`
         },
+        body: JSON.stringify({
+            title: parameters.title,
+            description: parameters.description,
+            startTime: parameters.startTime,
+            endTime: parameters.endTime,
+            location: parameters.location,
+            isPublic: parameters.isPublic,
+            ageRangeMax: parameters.ageRangeMax,
+            ageRangeMin: parameters.ageRangeMin,
+        })
     });
 
     if (!res.ok) {
